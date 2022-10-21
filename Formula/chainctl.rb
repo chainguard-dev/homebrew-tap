8dd65b8461bb6a9dfb3f7452c8cfbddb63886374
Josh Dolitsky <josh@dolit.ski>
2022-10-20 19:53:00 -0500

    Fix mismatched checksums
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- Formula/chainctl.rb
+++ Formula/chainctl.rb
@@ -15,19 +15,19 @@ class Chainctl < Formula
 
     if Hardware::CPU.arm?
       url "https://storage.googleapis.com/us.artifacts.prod-enforce-fabc.appspot.com/chainctl_Darwin_arm64"
-      sha256 "411e34dda772d79b6853cff2b6c75eb1fd769fedccf3dffb9e0ce7415d0be789"
+      sha256 "036a4a3211da7a4b2ac970a0213b8660fe2b8e2d5fe02af333f08b14f5d31d7d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://storage.googleapis.com/us.artifacts.prod-enforce-fabc.appspot.com/chainctl_Linux_x86_64"
-      sha256 "46148893a3dd5ed267780b8d838b4dda91d38f15e35b39c8081279c8fb94ef31"
+      sha256 "411e34dda772d79b6853cff2b6c75eb1fd769fedccf3dffb9e0ce7415d0be789"
     end
 
     if Hardware::CPU.arm?
       url "https://storage.googleapis.com/us.artifacts.prod-enforce-fabc.appspot.com/chainctl_Linux_arm64"
-      sha256 "036a4a3211da7a4b2ac970a0213b8660fe2b8e2d5fe02af333f08b14f5d31d7d"
+      sha256 "46148893a3dd5ed267780b8d838b4dda91d38f15e35b39c8081279c8fb94ef31"
     end
   end
 
