bb1dbc5305a37d3204ee25ae83705220be1ee4a7
Kenny Leung <kleung@chainguard.dev>
2022-12-16 11:44:02 -0800

    Update chainctl.rb

--- Formula/chainctl.rb
+++ Formula/chainctl.rb
@@ -13,7 +13,7 @@ class Chainctl < Formula
       sha256 "aefc6a9964b871413e3de3a95ef122080bac0b5740e436bc657f103065755769"
 
       def install
-        bin.install "chainctl_darwin_amd64" => "chainctl_darwin_amd64"
+        bin.install "chainctl_darwin_amd64" => "chainctl"
       end
     end
     if Hardware::CPU.arm?
@@ -21,7 +21,7 @@ class Chainctl < Formula
       sha256 "0765a44e8960c511e11fb0b39ea96002be6473cb8ad10ed1ade26fb351d3e439"
 
       def install
-        bin.install "chainctl_darwin_arm64" => "chainctl_darwin_arm64"
+        bin.install "chainctl_darwin_arm64" => "chainctl"
       end
     end
   end
@@ -32,7 +32,7 @@ class Chainctl < Formula
       sha256 "60ae3da047dac13f47f8c1c1c1f5b733f8dd6637a86afe1189b7306f3757121f"
 
       def install
-        bin.install "chainctl_linux_amd64" => "chainctl_linux_amd64"
+        bin.install "chainctl_linux_amd64" => "chainctl"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
@@ -40,7 +40,7 @@ class Chainctl < Formula
       sha256 "3c9d49262787eca036ed153825033af4bb85a72dda0ee51fa58cb0805537bb60"
 
       def install
-        bin.install "chainctl_linux_arm64" => "chainctl_linux_arm64"
+        bin.install "chainctl_linux_arm64" => "chainctl"
       end
     end
   end
